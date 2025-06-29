3e7e6a59dc40458c15b221f504c47571b0deb713
yassineaoukaili <yassine.aoukaili@gmail.com>
2025-06-29 18:16:16 +0200

    Update documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,13 +24,20 @@ author = 'Yassine A.'
 # The full version, including alpha/beta/rc tags
 release = '1.0.0'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx_rtd_theme",
+    'sphinx.ext.autodoc',
+    'sphinx_copybutton',
+    'nbsphinx',
+    "sphinxcontrib.rsvgconverter",
+    'sphinx.ext.mathjax',
+    'IPython.sphinxext.ipython_console_highlighting',
+    "sphinx_gallery.load_style"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -40,7 +47,6 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
-
 
 # -- Options for HTML output -------------------------------------------------
 
